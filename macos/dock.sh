5e4343ec3e2824b6f376a7f779f3834fd4bf1289
Ian Miller <irmiller22@gmail.com>
2016-10-22 11:40:43 -0400

    add correct applications to dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
-dockutil --no-restart --add "$HOME/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/Launchpad.app"
+dockutil --no-restart --add "/Applications/Siri.app"
+dockutil --no-restart --add "/Applications/App Store.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Messages.app"
+dockutil --no-restart --add "/Applications/FaceTime.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/Applications/Contacts.app"
+dockutil --no-restart --add "/Applications/Photos.app"
+dockutil --no-restart --add "/Applications/Notes.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Utilities/Console.app"
 
 killall Dock
