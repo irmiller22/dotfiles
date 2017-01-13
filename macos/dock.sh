ad4013f7517d43ce7423ba188c0c7b180f340fda
Ian Miller <irmiller22@gmail.com>
2017-01-13 09:24:10 -0500

    would rather not show POSIX path in Finder

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Launchpad.app"
 dockutil --no-restart --add "/Applications/Siri.app"
+dockutil --no-restart --add "/Applications/Launchpad.app"
+dockutil --no-restart --add "/Applications/iBooks.app"
 dockutil --no-restart --add "/Applications/App Store.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Messages.app"
 dockutil --no-restart --add "/Applications/FaceTime.app"
-dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/Contacts.app"
 dockutil --no-restart --add "/Applications/Photos.app"
 dockutil --no-restart --add "/Applications/Notes.app"
--- osxdefaults.sh
+++ osxdefaults.sh
@@ -6,7 +6,5 @@ defaults write com.apple.dock autohide -bool true
 defaults write com.apple.screencapture location -string “$HOME/Desktop”
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string “png”
-# Display full POSIX path as Finder window title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=” “
